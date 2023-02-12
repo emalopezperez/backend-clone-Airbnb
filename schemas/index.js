0ec179f302b0a608f0ead4f0ea732edcc44e8fec
emanuel lopez <emanuel-lopez13@hotmail.com>
2023-02-12 19:27:28 -0300

    Deploy del backend

---
+++ schemas/index.js
@@ -0,0 +1,16 @@
+import property from './property'
+import propertyImage from './propertyImage'
+import host from './host'
+import review from './review'
+import person from './person'
+import traveller from './traveller'
+
+export const schemaTypes = [
+  // Document types
+  property,
+  propertyImage,
+  host,
+  review,
+  person,
+  traveller
+]
